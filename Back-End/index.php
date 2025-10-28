469322a1fcd7ab65883ca8ba99ebc54f43d03356
khaled312001 <khaled312001@gmail.com>
2025-10-28 15:15:27 +0300

    Add CORS to root index.php

--- Back-End/index.php
+++ Back-End/index.php
@@ -5,6 +5,22 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// CORS Headers - Handle preflight requests FIRST
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '*';
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    if ($origin && $origin !== '*') {
+        header('Access-Control-Allow-Origin: ' . $origin);
+    } else {
+        header('Access-Control-Allow-Origin: *');
+    }
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, PATCH');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept, Origin, X-XSRF-TOKEN');
+    header('Access-Control-Max-Age: 86400');
+    http_response_code(200);
+    exit(0);
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
     require $maintenance;
@@ -17,4 +33,17 @@ require __DIR__.'/vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/bootstrap/app.php';
 
-$app->handleRequest(Request::capture()); 
+$response = $app->handleRequest(Request::capture());
+
+// Add CORS headers to response
+if ($origin && $origin !== '*') {
+    $response->headers->set('Access-Control-Allow-Origin', $origin);
+} else {
+    $response->headers->set('Access-Control-Allow-Origin', '*');
+}
+
+$response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+$response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin, X-XSRF-TOKEN');
+$response->headers->set('Access-Control-Max-Age', '86400');
+
+$response->send(); 
